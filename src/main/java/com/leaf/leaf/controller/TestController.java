1cb4e18a4525fa8db51953d3313df556b4ca09e7
k33ngTomide <Msodiqakintomide@gmail.com>
2024-06-05 11:50:08 +0100

    Added a new one

--- src/main/java/com/leaf/leaf/controller/TestController.java
+++ src/main/java/com/leaf/leaf/controller/TestController.java
@@ -2,6 +2,7 @@ package com.leaf.leaf.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -27,5 +28,10 @@ public class TestController {
     @GetMapping("/checkTested")
     public String testEnded(){
         return "Then I am sure this is working for real";
+    }
+
+    @PostMapping("/chec")
+    public String testEnded(String word){
+        return String.format("Then I am sure this is working for real %s", word);
     }
 }
